2235a21e2fbd55adf5357940d357d14d4d5bb9d0
CyronF <cyronpfraser@gmail.com>
2021-05-27 19:18:06 -0400

    edited code to save inputs

--- utils/questions.js
+++ utils/questions.js
@@ -1,8 +1,55 @@
 const questions = [
     {
-        name: "repoName",
-        message: "What's your github repos name?",
+        name: "title",
+        message: "What is the title of your project?"
     },
+
+    {
+        name: "description",
+        message: "Write a brief description of your project."
+    },
+
+    {
+        name:"installation",
+        message:"Please describe the installation process."
+    },
+
+    {
+         name:"useage",
+         message:"Briefly describe the useage of this project."
+    },
+
+    {
+        name:"contributing",
+        message:"Please include contributing guidelines for this project."
+    },
+    
+    {       
+        name:"test",
+        message:"Write your test instructions."
+    },
+    
+    {
+        type: "checkbox",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+    }
+        
+
+        
+
+
+
+    
 ];
 
 module.exports = questions;
